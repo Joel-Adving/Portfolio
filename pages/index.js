55bd05238f0a4be427df0bf0a5515a9e1a8987d3
Joel-Adving <joel.adving@gmail.com>
2021-03-24 14:41:43 +0100

    removed href to old portfolio

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ export default function Home() {
     <>
       <div className="flex bg-gray-800 w-screen h-screen flex-col justify-center">
         <a
-          href="https://joeladving.com/"
+          href="#"
           className="text-gray-300 font-bold text-4xl bg-gray-700 mx-auto rounded px-4 py-2 pb-3 text-center"
         >
-          <h1>Joel Adving | Protfolio v3</h1>
+          <h1>Joel Adving - Protfolio v3</h1>
         </a>
         <p className="text-gray-500 mx-auto mt-4 text-center">Coming Soon...</p>
       </div>

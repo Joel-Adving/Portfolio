90d367ede65e8f9c416856e4722338c7cfc0820e
Joel-Adving <joel.adving@gmail.com>
2021-03-31 17:28:03 +0100

    dev

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -7,10 +7,10 @@ const Projects = () => {
         <h1 className=" text-5xl my-16  md:my-24 self-center font-semibold text-main-black underline">
           PROJECTS
         </h1>
-        <div className="flex flex-col md:flex-row flex-wrap items-center justify-center max-w-xl">
+        <div className="flex flex-col lg:flex-row flex-wrap items-center justify-center">
           {/* <div className="flex flex-wrap justify-center self-center mb-4"> */}
           <ProjectCard title="Netflix UI" img="images/netflix.png" description="Lorem aoiwdjaiuwdiuawhdiuwh"/>
-          <ProjectCard title="Live Chat" img="images/chat.png" description="Lorem aoiwdjaiuwdiuawhdiuwh"/>
+          <ProjectCard title="Live Chat" link="https://chatroom-app-3f006.web.app/" img="images/chat.png" description="Lorem aoiwdjaiuwdiuawhdiuwh"/>
           <ProjectCard title="Deocument Text Edtior " img="images/quire.png" description="Lorem aoiwdjaiuwdiuawhdiuwh"/>
         </div>
       </div>
--- components/Projects/components/ProjectCard.js
+++ components/Projects/components/ProjectCard.js
@@ -1,7 +1,9 @@
 const ProjectCard = (props) => {
   return (
     <div className="my-4">
+    <a href={props.link}>
       <img className=" max-w-sm" src={props.img} alt="" height="auto" wdith="auto" />
+    </a>
       <div className="">{props.title}</div>
       <div>{props.description}</div>
     </div>

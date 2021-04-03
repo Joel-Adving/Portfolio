b5840689409902435b9cb3b052b14f8d80ecf7ec
Joel-Adving <joel.adving@gmail.com>
2021-04-04 01:37:45 +0200

    dev

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
-import Background from "../components/Background"
-import Hero from "../components/Hero"
-import Header from "../components/Header"
-import About from "../components/About"
-import Contact from "../components/Contact"
-import Footer from "../components/Footer"
-import Projects from "../components/Projects"
+import Background from '../components/Background'
+import Hero from '../components/Hero'
+import Header from '../components/Header'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import Projects from '../components/Projects'
 import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <html lang="en" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="description" content="My front end development portfolio."/>
+        <meta
+          name="description"
+          content="My front end development portfolio."
+        />
         <meta name="robots" content="index, follow" />
-        <title>Joel Adving</title> 
+        <title>Joel Adving</title>
       </Head>
       <main className="flex flex-col bg-main-black overflow-x-hidden">
         <Background />

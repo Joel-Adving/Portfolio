d76c2cb07d09d943da883e7c13fc941be6fb8e70
Joel Adving <joel.adving@gmail.com>
2021-04-06 13:54:42 +0200

    main

--- pages/components/Projects/Projects.js
+++ pages/components/Projects/Projects.js
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <section className="flex flex-col z-10 px-4 bg-discordTheme-light md:pb-36 text-main-white">
       <div className="flex flex-col self-center">
-        <h1 className=" text-5xl my-16  md:my-24 self-center font-semibold text-main-white underline">
+        <h1 className=" text-5xl my-16  md:my-24 self-center font-semibold text-main-white">
           PROJECTS
         </h1>
         <div className="flex flex-col lg:flex-row flex-wrap items-center justify-center">
